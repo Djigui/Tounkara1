4ec3ea4c5cc467654f985489c1b584402d3912f2
Djigui Tounkara <tounkaradjg@gmail.com>
2021-07-22 20:37:26 +0200

    esercitazione condizionali

---
+++ 01-esercizione.js
@@ -0,0 +1,37 @@
+let bas = 2;
+for(let i = bas; i < 20003; i++) {
+  console.log(i);
+  }
+  let num1 = 12;
+  let num2 = 20;
+  let jam1 = 7;
+  let blackn = 13;
+  console.log("${}")
+
+const pc = {
+marca: "lenovo",
+modello: "idealpad flex 5",
+schermo: "15 police",
+ssd: "225 gb ",
+ram: "8 gb",
+schermo2: "tous",
+batteria: " 25 ore",
+infoSulPc: "ideal pad è uno dei computer più potente del undicessimo generazione",
+}
+console.log(pc);
+let flex = 20;
+let a1 = 22;
+let za = 31;
+if(flex == a1, flex == za) {
+  console.log('buon anno');
+} 
+if( a1 == za, a1 != flex ){
+  console.log( 'va bene allora mi hai convinto');
+}
+if (za <= flex, za >= a1) {
+  console.log('bene bene vedo che ha funzionato il mio codice perche sono lunico ');
+
+} 
+else {
+  console.log('allora tutti altrinativi non hanno funzionato');
+}
