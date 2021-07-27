f0f7bb653f660f50da67db1cfbd440047fbdd4b8
Djigui Tounkara <tounkaradjg@gmail.com>
2021-07-27 19:26:59 +0200

    multipli di un numero fino 10

--- malin/foo.js
+++ malin/foo.js
@@ -1,2 +1,14 @@
-document.getElementsByClassName("samba");
-console.log(document.getElementsByClassName);
+const nMultiplisof = (number, limit) => {
+    let multiplies = [];
+    let i = 1;
+    while(multiplies.length <= limit) {
+
+    if(i % number === 0) {
+
+        multiplies.push(i);
+    }
+    i++;
+}
+return multiplies;
+} 
+console.log(nMultiplisof(4, 200));
